3a6208bf33e49b30c9b081bb626b659027a42c42
Lukas Pseja <luk4sps3j4@gmail.com>
2024-04-03 13:48:44 +0200

    added comments and formatting

--- htab_private.h
+++ htab_private.h
@@ -1,12 +1,14 @@
 #include "htab.h"
 
-struct htab_item {
-    htab_pair_t pair;
-    struct htab_item *next;
+struct htab_item
+{
+    htab_pair_t pair;       // (key, data) pair
+    struct htab_item *next; // pointer to the next htab_item in the array
 };
 
-struct htab {
-    size_t size;
-    size_t arr_size;
-    struct htab_item **ptr_array;
+struct htab
+{
+    size_t size;                  // number of htab_items in the table
+    size_t arr_size;              // number of pointers in the htab_item ptr_array
+    struct htab_item **ptr_array; // flexible array member
 };
