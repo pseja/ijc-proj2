031519922f6c234e641801e637409df8a52c4e10
Lukas Pseja <luk4sps3j4@gmail.com>
2024-04-03 17:05:09 +0200

    better testing -> assert and debug macro printing

--- wordcount.c
+++ wordcount.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "htab.h"
 
+#define DEBUG
+#ifdef DEBUG
+#include <assert.h> // TODO for testing only, delete after done testing
+#endif
+
 #define HASH_TABLE_SIZE 69 // temporary value
 
 int main()
@@ -13,21 +18,28 @@ int main()
         return 1;
     }
 
+#ifdef DEBUG
     printf("🔥this is fine🔥\n");
     printf("htab_size: %zu\n", htab_size(hash_table));
+    assert(htab_size(hash_table) == 0);
     printf("htab_bucket_count: %zu\n", htab_bucket_count(hash_table));
+    assert(htab_bucket_count(hash_table) == HASH_TABLE_SIZE);
 
     printf("\nAdding text to hash table...\n");
     htab_pair_t *something1 = htab_lookup_add(hash_table, "something");
     printf("htab_size: %zu\n", htab_size(hash_table));
+    assert(htab_size(hash_table) == 1);
 
     printf("Adding another text to hash table...\n");
     htab_pair_t *else1 = htab_lookup_add(hash_table, "else");
     printf("htab_size: %zu\n", htab_size(hash_table));
+    assert(htab_size(hash_table) == 2);
 
     printf("Adding existing text to hash table...\n");
     htab_pair_t *something2 = htab_lookup_add(hash_table, "something");
     printf("htab_size: %zu\n", htab_size(hash_table));
+    assert(htab_size(hash_table) == 2);
+#endif
 
     // read words from stdin
 
