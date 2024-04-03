543a4343598dc42209d39b807a9334f6eec68b85
Lukas Pseja <luk4sps3j4@gmail.com>
2024-04-03 15:40:44 +0200

    sdbm hash function

---
+++ htab_hash_function.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include "htab_private.h"
+
+// sdbm hash function from: http://www.cse.yorku.ca/~oz/hash.html
+size_t htab_hash_function(const char *str)
+{
+    uint32_t h = 0; // musí mít 32 bitů
+
+    const unsigned char *p;
+    for (p = (const unsigned char *)str; *p != '\0'; p++)
+        h = 65599 * h + *p;
+    return h;
+}
