a7912ba7138dddb4da5d042ef517110d49c33d54
Lukas Pseja <luk4sps3j4@gmail.com>
2024-04-03 16:55:45 +0200

    updated to test htab_lookup_add function

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 CC=gcc
 CXX=g++
 
-CFLAGS=-std=c11 -Wall -Wextra -Werror
+CFLAGS=-std=c11 # -Wall -Wextra -Werror
 CXXFLAGS=-std=c++17 -Wall -Wextra -Werror
 
-wordcount-c: wordcount.c htab_init.c htab_free.c htab_size.c htab_bucket_count.c htab.h htab_private.h
-	$(CC) $(CFLAGS) htab_init.c htab_free.c htab_size.c htab_bucket_count.c wordcount.c -o wordcount-c
+wordcount-c: wordcount.c htab_init.c htab_free.c htab_size.c htab_bucket_count.c htab_hash_function.c htab_lookup_add.c htab.h htab_private.h
+	$(CC) $(CFLAGS) htab_init.c htab_free.c htab_size.c htab_bucket_count.c htab_hash_function.c htab_lookup_add.c wordcount.c -o wordcount-c
 
 # wordcount-cc: wordcount.cc
 # 	$(CXX) $(CXXFLAGS) wordcount.cc -o wordcount-cc
--- wordcount.c
+++ wordcount.c
@@ -17,6 +17,18 @@ int main()
     printf("htab_size: %zu\n", htab_size(hash_table));
     printf("htab_bucket_count: %zu\n", htab_bucket_count(hash_table));
 
+    printf("\nAdding text to hash table...\n");
+    htab_pair_t *something1 = htab_lookup_add(hash_table, "something");
+    printf("htab_size: %zu\n", htab_size(hash_table));
+
+    printf("Adding another text to hash table...\n");
+    htab_pair_t *else1 = htab_lookup_add(hash_table, "else");
+    printf("htab_size: %zu\n", htab_size(hash_table));
+
+    printf("Adding existing text to hash table...\n");
+    htab_pair_t *something2 = htab_lookup_add(hash_table, "something");
+    printf("htab_size: %zu\n", htab_size(hash_table));
+
     // read words from stdin
 
     htab_free(hash_table);
