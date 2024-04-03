eb142115659d5e55a1694dcc6b14160e3276be69
Lukas Pseja <luk4sps3j4@gmail.com>
2024-04-03 15:10:41 +0200

    formatting

--- wordcount.cc
+++ wordcount.cc
@@ -8,10 +8,11 @@
 #include <iostream>
 #include <unordered_map>
 
-int main() {
+int main()
+{
     using namespace std;
-    unordered_map<string,int> m;  // asociativní pole
-                    // mapuje klíč/string na hodnotu/int
+    unordered_map<string, int> m; // asociativní pole
+                                  // mapuje klíč/string na hodnotu/int
     string word;
     while (cin >> word) // čtení slova (jako scanf "%s", ale bezpečné)
         m[word]++;      // počítání výskytů slova (zvýší hodnotu pro
@@ -19,8 +20,8 @@ int main() {
                         // jinak vytvoří nový záznam s hodnotou 0 a
                         // tu operace ++ zvýší na 1)
 
-    for (auto &mi: m)   // pro všechny prvky kontejneru m
+    for (auto &mi : m) // pro všechny prvky kontejneru m
         cout << mi.first << "\t" << mi.second << "\n";
-        //      klíč/slovo          hodnota/počet
-        // prvky kontejneru typu "map" jsou dvojice (klíč,hodnota)
+    //      klíč/slovo          hodnota/počet
+    // prvky kontejneru typu "map" jsou dvojice (klíč,hodnota)
 }
