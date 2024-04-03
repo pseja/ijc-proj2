80ece8a9039d5ec5bcd9c90eff9de6f78a04982b
Lukas Pseja <luk4sps3j4@gmail.com>
2024-04-03 20:24:04 +0200

    implemented htab_erase function

---
+++ htab_erase.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "htab.h"
+#include "htab_private.h"
+
+bool htab_erase(htab_t *t, htab_key_t key)
+{
+    size_t index = htab_hash_function(key) % t->arr_size;
+    struct htab_item *current_item = t->ptr_array[index];
+    struct htab_item *previous_item = NULL;
+
+    while (current_item != NULL)
+    {
+        if (strcmp(current_item->pair->key, key) == 0)
+        {
+            // if the item is the first one, set the pointer to the next item
+            if (previous_item == NULL)
+            {
+                t->ptr_array[index] = current_item->next;
+            }
+            // if the item is not the first one, point the previous item to the next one
+            else
+            {
+                previous_item->next = current_item->next;
+            }
+
+            free((void *)current_item->pair->key);
+            free(current_item->pair);
+            free(current_item);
+
+            t->size--;
+
+            // the pair got removed
+            return true;
+        }
+
+        previous_item = current_item;
+        current_item = current_item->next;
+    }
+
+    // the pair was not removed due to not existing
+    return false;
+}
