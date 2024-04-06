26e85e0d200639c0542c3d76afed85a2ad7af999
Lukas Pseja <luk4sps3j4@gmail.com>
2024-04-06 19:20:53 +0200

    better arguments handling

--- tail.c
+++ tail.c
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define MAX_LINE_LENGTH 2047
+
+// error codes
+#define ERR_ARGS 1
+#define ERR_FILE 2
+#define ERR_INTENDED_EXIT -1
+
+typedef struct
+{
+    FILE *file;
+    int num_of_lines;
+} Arguments;
 
 typedef struct
 {
@@ -23,6 +35,74 @@ char *duplicate_string(char *line);
 void cbuf_put(CircularBuffer *cbuf, char *line);
 char *cbuf_get(CircularBuffer *cbuf, int index);
 void cbuf_print(CircularBuffer *cbuf);
+
+int handle_arguments(Arguments *args, int argc, char **argv)
+{
+    args->file = NULL;
+    args->num_of_lines = 10;
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-n") == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                fprintf(stderr, "Invalid amount of arguments\n");
+                return ERR_ARGS;
+            }
+
+            if (strspn(argv[++i], "-0123456789") != strlen(argv[i]))
+            {
+                fprintf(stderr, "-n [NUMBER] - NUMBER is not a valid number\n");
+                return ERR_ARGS;
+            }
+
+            args->num_of_lines = atoi(argv[i]);
+
+            if (args->num_of_lines == 0)
+            {
+                return ERR_INTENDED_EXIT;
+            }
+            else if (args->num_of_lines < 0)
+            {
+                args->num_of_lines *= -1;
+            }
+        }
+        else if (args->file == NULL)
+        {
+            args->file = fopen(argv[i], "r");
+
+            if (args->file == NULL)
+            {
+                fprintf(stderr, "Couldn't open file %s\n", argv[i]);
+                return ERR_FILE;
+            }
+
+            // check if file is empty
+            int c = fgetc(args->file);
+            if (c == EOF)
+            {
+                return ERR_INTENDED_EXIT;
+            }
+            else
+            {
+                ungetc(c, args->file);
+            }
+        }
+        else
+        {
+            fprintf(stderr, "Invalid amount of arguments\n");
+            return ERR_ARGS;
+        }
+    }
+
+    if (args->file == NULL)
+    {
+        args->file = stdin;
+    }
+
+    return 0;
+}
 
 bool cbuf_empty(CircularBuffer *cbuf)
 {
@@ -111,60 +191,9 @@ void cbuf_print(CircularBuffer *cbuf)
     printf("\n");
 }
 
-int main(int argc, char **argv)
+void read_lines(CircularBuffer *cbuf, FILE *file, unsigned max_line_length)
 {
-    FILE *file = NULL;
-    int num_of_lines = 10;
-
-    for (int i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-n") == 0)
-        {
-            if (i + 1 >= argc)
-            {
-                fprintf(stderr, "Invalid amount of arguments\n");
-                return 1;
-            }
-
-            num_of_lines = atoi(argv[++i]);
-
-            if (num_of_lines <= 0)
-            {
-                fprintf(stderr, "Invalid number of lines\n");
-                return 1;
-            }
-        }
-        else if (file == NULL)
-        {
-            file = fopen(argv[i], "r");
-
-            if (file == NULL)
-            {
-                fprintf(stderr, "Couldn't open file %s\n", argv[i]);
-                return 1;
-            }
-        }
-        else
-        {
-            fprintf(stderr, "Invalid amount of arguments\n");
-            return 1;
-        }
-    }
-
-    if (file == NULL)
-    {
-        file = stdin;
-    }
-
-    CircularBuffer *cbuf = cbuf_create(num_of_lines);
-
-    if (cbuf == NULL)
-    {
-        fprintf(stderr, "Couldn't create circular buffer\n");
-        return 1;
-    }
-
-    char line[MAX_LINE_LENGTH + 2];
+    char line[max_line_length + 2];
     bool line_too_long_flag = false;
 
     while (fgets(line, MAX_LINE_LENGTH + 2, file) != NULL)
@@ -181,15 +210,40 @@ int main(int argc, char **argv)
         }
         cbuf_put(cbuf, line);
     }
+}
+
+int main(int argc, char **argv)
+{
+    Arguments args;
+    int err = handle_arguments(&args, argc, argv);
+
+    if (err == ERR_INTENDED_EXIT)
+    {
+        return 0;
+    }
+    else if (err != 0)
+    {
+        return err;
+    }
+
+    CircularBuffer *cbuf = cbuf_create(args.num_of_lines);
+
+    if (cbuf == NULL)
+    {
+        fprintf(stderr, "Couldn't create circular buffer\n");
+        return 1;
+    }
+
+    read_lines(cbuf, args.file, MAX_LINE_LENGTH);
 
     cbuf_print(cbuf);
 
     cbuf_free(cbuf);
 
-    if (file != stdin && fclose(file) != 0)
+    if (args.file != stdin && fclose(args.file) != 0)
     {
         fprintf(stderr, "Couldn't close file\n");
-        return 1;
+        return ERR_FILE;
     }
 
     return 0;
